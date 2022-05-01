39f93e2f0361f5366aa9c71fa37142e6da9e3cda
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-05-01 11:07:02 +0800

    add origin files

---
+++ kubejs/startup_scripts/custom_stuff.js
@@ -0,0 +1,29 @@
+// Custom Fluid
+onEvent('fluid.registry', event => {
+  // Molten Coke Brick
+  event.create('molten_coke_brick').textureThick(0x292929).bucketColor(0x292929).displayName('Molten Coke Brick')
+
+  // Molten Shadow Steel
+  event.create('molten_shadow_steel').textureThick(0x4B006E).bucketColor(0x4B006E).displayName('Molten shadow steel')
+})
+
+// Custom Items
+onEvent('item.registry', event => {
+  event.create('boots').displayName('Boots Core')
+  event.create('leggings').displayName('Leggings Core')
+  event.create('chestplate').displayName('Chestplate Core')
+  event.create('helmet').displayName('Helmet Core')
+  event.create('damage').displayName('Damage Core')
+
+  event.create('chroma_ingot').displayName('Chroma Ingot')
+  event.create('unassembled_chroma_ingot').displayName('Unassembled Chroma Ingot')
+  event.create('condensed_magical_ingot').displayName('Condensed Magical Ingot')
+  event.create('assembled_technical_ingot').displayName('Dense Technical Ingot')
+
+  event.create('elemental_essence').displayName('Elemental Essence')
+  event.create('fallen_pet').displayName('Shard Of The Fallen Pet')
+  event.create('pure_evil').displayName('Essence Of Pure Evil')
+  event.create('condensed_mana').displayName('Condensed Mana')
+
+  event.create('vous_verrez_en_temps_voulu').displayName('Vous Verrez En Temps Voulu')
+})
